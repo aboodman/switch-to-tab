ff0948ab9d08d3ff11a121b37542dbaef9106490
jmfayard <jean-michel.fayard@smartmobilefactory.com>
2015-02-04 15:11:17 +0100

    I would like that tab<TAB><ENTER> jump to the last select tab

--- options.js
+++ options.js
@@ -1,7 +1,10 @@
 // Saves options to localStorage.
 function save_options() {
   var autoswitch = document.getElementById("autoswitch").checked;
+  var emptyswitch = document.getElementById("emtpyswitch").checked;
+
   localStorage["autoswitch"] = autoswitch;
+  localStorage["emptyswitch"] = emptyswitch;
 
   // Update status to let user know options were saved.
   var status = document.getElementById("status");
@@ -13,7 +16,7 @@ function save_options() {
 
 // Restores select box state to saved value from localStorage.
 function restore_options() {
-  var autoswitch = localStorage["autoswitch"]; 
+  var autoswitch = localStorage["autoswitch"];
 
   document.getElementById("autoswitch").checked = (""+autoswitch) == "true";
 }
